5794c79b376df3c468e6554ceda186b08f6639b8
bpinsky123 <43283231+bpinsky123@users.noreply.github.com>
2026-09-06 10:57:52 -0700

    Fix capability setup CLI flags

--- drivers/SmartThings/zwave-lock/custom_capabilities/create.sh
+++ drivers/SmartThings/zwave-lock/custom_capabilities/create.sh
@@ -18,12 +18,12 @@ schlageInteriorButton
 
 for name in $capabilities; do
   id="$namespace.$name"
-  if smartthings capabilities "$id" --organization "$organization" --capability-version 1 --json >/dev/null 2>&1; then
+  if smartthings capabilities "$id" --capability-version 1 --json >/dev/null 2>&1; then
     smartthings capabilities:update "$id" 1 --organization "$organization" -i "$root/$id.capability.json"
   else
     smartthings capabilities:create --namespace "$namespace" --organization "$organization" -i "$root/$id.capability.json"
   fi
-  if smartthings capabilities:presentation "$id" --organization "$organization" --capability-version 1 --json >/dev/null 2>&1; then
+  if smartthings capabilities:presentation "$id" --capability-version 1 --json >/dev/null 2>&1; then
     smartthings capabilities:presentation:update "$id" 1 --organization "$organization" -i "$root/$id.presentation.json"
   else
     smartthings capabilities:presentation:create "$id" --organization "$organization" --capability-version 1 -i "$root/$id.presentation.json"
