b2103d22dd81b4fa0fc35dec41fad871f23da893
bpinsky123 <43283231+bpinsky123@users.noreply.github.com>
2026-09-05 20:54:43 -0700

    Make capability script POSIX shell compatible

--- drivers/SmartThings/zwave-lock/custom_capabilities/create.sh
+++ drivers/SmartThings/zwave-lock/custom_capabilities/create.sh
@@ -1,21 +1,21 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/bin/sh
+set -eu
 
 root="$(cd "$(dirname "$0")" && pwd)"
 namespace="heartsample19211"
 
-capabilities=(
-  lockActivity
-  deviceNetworkId
-  schlageLockAlarm
-  autoLock
-  lockAndLeave
-  vacationMode
-  keypadBeep
-  schlageInteriorButton
-)
+capabilities="
+lockActivity
+deviceNetworkId
+schlageLockAlarm
+autoLock
+lockAndLeave
+vacationMode
+keypadBeep
+schlageInteriorButton
+"
 
-for name in "${capabilities[@]}"; do
+for name in $capabilities; do
   id="${namespace}.${name}"
   smartthings capabilities:create -i "${root}/${id}.capability.json"
   smartthings capabilities:presentation:create "$id" 1 -i "${root}/${id}.presentation.json"
