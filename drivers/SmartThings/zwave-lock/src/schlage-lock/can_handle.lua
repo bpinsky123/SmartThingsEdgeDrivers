1ad9496312893605dbfdf93318d20e3ab5dd7ef0
bpinsky123 <43283231+bpinsky123@users.noreply.github.com>
2026-09-13 12:34:52 -0700

    Update can_handle.lua

--- drivers/SmartThings/zwave-lock/src/schlage-lock/can_handle.lua
+++ drivers/SmartThings/zwave-lock/src/schlage-lock/can_handle.lua
@@ -1,15 +1,25 @@
 -- Copyright 2026 SmartThings, Inc.
 -- Licensed under the Apache License, Version 2.0
 
+local capabilities = require "st.capabilities"
+local constants = require "lock_utils.constants"
+
+local function is_slga_migrated(device)
+  return device:get_field(constants.DRIVER_STATE.SLGA_MIGRATED) == true
+    or device:get_latest_state(
+      "main",
+      capabilities.lockCodes.ID,
+      capabilities.lockCodes.migrated.NAME
+    ) == true
+end
+
 return function(opts, driver, device, cmd)
-  local consts = require("lock_utils.constants")
-  local slga_migrated = device:get_field(consts.DRIVER_STATE.SLGA_MIGRATED)
-  if slga_migrated then
-    local SCHLAGE_MFR = 0x003B
-    if device.zwave_manufacturer_id == SCHLAGE_MFR then
-      local subdriver = require("schlage-lock")
-      return true, subdriver
-    end
+  if not is_slga_migrated(device) then
+    return false
+  end
+
+  if device.zwave_manufacturer_id == 0x003B then
+    return true, require "schlage-lock"
   end
   return false
 end
