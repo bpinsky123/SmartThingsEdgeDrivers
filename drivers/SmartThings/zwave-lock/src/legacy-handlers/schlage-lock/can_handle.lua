1943974a86e272d54cb89f023188376c88847061
bpinsky123 <43283231+bpinsky123@users.noreply.github.com>
2026-09-13 12:30:02 -0700

    Route migrated Schlage locks out of legacy handlers

--- drivers/SmartThings/zwave-lock/src/legacy-handlers/schlage-lock/can_handle.lua
+++ drivers/SmartThings/zwave-lock/src/legacy-handlers/schlage-lock/can_handle.lua
@@ -1,7 +1,13 @@
 -- Copyright 2025 SmartThings, Inc.
 -- Licensed under the Apache License, Version 2.0
 
+local constants = require "lock_utils.constants"
+
 return function(opts, driver, device, cmd)
+  if device:get_field(constants.DRIVER_STATE.SLGA_MIGRATED) == true then
+    return false
+  end
+
   local SCHLAGE_MFR = 0x003B
   if device.zwave_manufacturer_id == SCHLAGE_MFR then
     local subdriver = require("legacy-handlers.schlage-lock")
