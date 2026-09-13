5c4c39535e2f49da030ddb496105b309776fddf5
bpinsky123 <43283231+bpinsky123@users.noreply.github.com>
2026-09-13 12:35:20 -0700

    Use capability migration state for legacy routing

--- drivers/SmartThings/zwave-lock/src/legacy-handlers/schlage-lock/can_handle.lua
+++ drivers/SmartThings/zwave-lock/src/legacy-handlers/schlage-lock/can_handle.lua
@@ -1,17 +1,25 @@
 -- Copyright 2025 SmartThings, Inc.
 -- Licensed under the Apache License, Version 2.0
 
+local capabilities = require "st.capabilities"
 local constants = require "lock_utils.constants"
 
+local function is_slga_migrated(device)
+  return device:get_field(constants.DRIVER_STATE.SLGA_MIGRATED) == true
+    or device:get_latest_state(
+      "main",
+      capabilities.lockCodes.ID,
+      capabilities.lockCodes.migrated.NAME
+    ) == true
+end
+
 return function(opts, driver, device, cmd)
-  if device:get_field(constants.DRIVER_STATE.SLGA_MIGRATED) == true then
+  if is_slga_migrated(device) then
     return false
   end
 
-  local SCHLAGE_MFR = 0x003B
-  if device.zwave_manufacturer_id == SCHLAGE_MFR then
-    local subdriver = require("legacy-handlers.schlage-lock")
-    return true, subdriver
+  if device.zwave_manufacturer_id == 0x003B then
+    return true, require "legacy-handlers.schlage-lock"
   end
   return false
 end
