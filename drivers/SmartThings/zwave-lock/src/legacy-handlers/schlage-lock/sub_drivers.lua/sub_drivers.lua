c839cf019b22a3364ee0a25e759c936e3c7621ae
bpinsky123 <43283231+bpinsky123@users.noreply.github.com>
2026-09-15 12:02:24 -0700

    Remove misplaced Schlage subdriver manifest

--- drivers/SmartThings/zwave-lock/src/legacy-handlers/schlage-lock/sub_drivers.lua/sub_drivers.lua
+++
@@ -1,8 +0,0 @@
--- Copyright 2025 SmartThings, Inc.
--- Licensed under the Apache License, Version 2.0
-
-local lazy_load_if_possible = require "lazy_load_subdriver"
-
-return {
-  lazy_load_if_possible("legacy-handlers.schlage-lock.schlage-lock-bp"),
-}
